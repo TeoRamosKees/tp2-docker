639a6e113846dd98460ae6682d1c886a04ea6a52
RenzoRomeo <renzoromeo44@gmail.com>
2024-05-26 19:15:01 -0300

    fix: cors

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 import express from "express";
+import cors from "cors";
 import booksRouter from "./routes/books.js";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
-const port = 3000;
 
 app.use("/books/", booksRouter);
 
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
